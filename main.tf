9771f0b52825a000572202620fb5706e0e6cf104
Wizardlyfix <canelafix@gmail.com>
2024-11-03 13:04:53 -0500

    main.tf en funcionamiento

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 
 # Resource
 
-resource "aws_instance" "example" {
+resource "aws_instance" "nginx-server" {
   ami           = "ami-0440d3b780d96b29d"
   instance_type = "t3.micro"
 }

2865ed5090c994df766cb40091ef2ca31800d414
Wizardlyfix <canelafix@gmail.com>
2024-11-03 15:42:50 -0500

    Agregar Outputs

--- main.tf
+++ main.tf
@@ -87,3 +87,21 @@ resource "aws_security_group" "nginx-server-sg" {
     Project = "Tarea"
   }
 }
+
+output "public_ip" {
+  description = "Dirección IP pública de la instancia EC2"
+  value = aws_instance.nginx-server.public_ip
+}
+
+output "server_public_dns" {
+  description = "DNS público de la instancia EC2"
+  value = aws_instance.nginx-server.public_dns
+}
+
+#terraform output:
+
+# curl http://52.90.248.59
+
+#Para conectarse por SSH:
+
+# ssh ec2-user@52.90.248.59 -i ./nginx-server.key
